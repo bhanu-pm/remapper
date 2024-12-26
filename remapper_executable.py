24cefbb9d0dc7b37c7b0ea9f01116b1d0926aaa8
bhanu-pm <bhanuprakashm1108@gmail.com>
2024-12-25 20:29:30 -0700

    Final commit

--- remapper_executable.py
+++ remapper_executable.py
@@ -1,3 +1,4 @@
+# Working prototype that switches between two desktops in windows
 import keyboard
 import time
 
@@ -16,17 +17,30 @@ def simulate_shortcut(shortcut):
         keyboard.release(key)  # Release the key in the shortcut
 
 # Listen for the custom key press and simulate a shortcut when pressed
-print("Listening for custom key press (keycode: {})...".format(CUSTOM_KEYCODE))
+# print("Listening for custom key press (keycode: {})...".format(CUSTOM_KEYCODE))
+
+prev = 0
 while True:
     event = keyboard.read_event()  # Capture the key event
 
     # Check if the custom key is pressed based on its keycode
     if event.event_type == keyboard.KEY_DOWN and event.scan_code == CUSTOM_KEYCODE:
-        print("Custom key pressed! Simulating shortcut...")
-        simulate_shortcut(KEYBOARD_SHORTCUT)  # Simulate the keyboard shortcut
-        time.sleep(0.5)  # Sleep to prevent multiple presses in quick succession
+        # print("Custom key pressed! Simulating shortcut...")
+        # print(f"prev = {prev}")
+        if prev == 1:
+            simulate_shortcut(KEYBOARD_SHORTCUT[2])  # Simulate the keyboard shortcut 2
+            time.sleep(0.5)  # Sleep to prevent multiple presses in quick succession
+            prev = 2
+        elif prev == 2:
+            simulate_shortcut(KEYBOARD_SHORTCUT[1])  # Simulate the keyboard shortcut 1
+            time.sleep(0.5)  # Sleep to prevent multiple presses in quick succession
+            prev = 1
+        elif prev == 0:
+            simulate_shortcut(KEYBOARD_SHORTCUT[1])  # Simulate the keyboard shortcut 1
+            time.sleep(0.5)  # Sleep to prevent multiple presses in quick succession
+            prev = 1
     
     # Optionally, exit the loop by pressing the 'esc' key
-    if event.name == 'esc':
-        print("Exiting the script.")
-        break
+    # if event.name == 'esc':
+        # print("Exiting the script.")
+        # break
